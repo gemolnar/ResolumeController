84da1a4bcbf05bf05d43be8bf5d406ee6a2b2c82
Gergő Molnár <gXXXXXX@gmail.com>
2017-04-04 09:31:21 +0200

    Initial commit

---
+++ ResolumeController/Program.cs
@@ -0,0 +1,109 @@
+﻿using Rug.Osc;
+using Sanford.Multimedia;
+using Sanford.Multimedia.Midi;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResolumeController
+{
+    //class OscState
+    //{
+    //    List<object> _values = new List<object>();
+    //    public List<object> Values {  get { return _values; } }
+    //    public int Count { get; set; }
+    //}
+
+
+    class MidiReader : IDisposable
+    {
+        private InputDevice _inDevice = null;
+
+        public MidiReader(int midiDeviceId)
+        {
+            _inDevice = new InputDevice(midiDeviceId);
+            _inDevice.ChannelMessageReceived += OnChannelMessageReceived;
+            _inDevice.MessageReceived += OnMessageReceived;
+            _inDevice.ShortMessageReceived += OnShortMessageReceived;
+            _inDevice.StartRecording();
+        }
+
+        private void OnShortMessageReceived(object sender, ShortMessageEventArgs e)
+        {
+            var logMessage = $"{e.Message.Message} (ShortMessage of type {e.Message.MessageType}, status {e.Message.Status}).";
+            //Console.WriteLine(logMessage);
+        }
+
+        private void OnMessageReceived(IMidiMessage message)
+        {
+        }
+
+        private void OnChannelMessageReceived(object sender, ChannelMessageEventArgs e)
+        {
+            string logMessage;
+            switch(e.Message.Command)
+            {
+                case ChannelCommand.NoteOn:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Note n = (Note)(e.Message.Data1 % 12);
+                    logMessage = $"NoteOn >{n} (Velocity: {e.Message.Data2}, CH#{e.Message.MidiChannel}).";
+                    break;
+                case ChannelCommand.NoteOff:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    logMessage = $"NoteOff>{e.Message.Data1} (Velocity: {e.Message.Data2}, CH#{e.Message.MidiChannel}).";
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    logMessage = $"CH#{e.Message.MidiChannel} Command: {e.Message.Command}, Data1: {e.Message.Data1}, Data2: {e.Message.Data2}).";
+                    break;
+            }
+            Console.WriteLine(logMessage);
+        }
+
+        public static IEnumerable<string> GetDevices()
+        {
+            for (int deviceIndex = 0; deviceIndex < InputDevice.DeviceCount; deviceIndex++)
+            {
+                var deviceInfo = InputDevice.GetDeviceCapabilities(deviceIndex);
+                yield return $"[ID={deviceIndex}] \"{deviceInfo.name}\", V{deviceInfo.driverVersion / 256}.{deviceInfo.driverVersion % 256}";
+            } 
+        }
+
+        public void Dispose()
+        {
+            _inDevice.StartRecording();
+            _inDevice.Dispose();
+        }
+    }
+
+    class Program
+    {
+        private static Resolume _resolume;
+        private static MidiReader _abletonIn;
+
+        static void Main(string[] args)
+        {
+            foreach (var deviceName in MidiReader.GetDevices())
+            {
+                Console.WriteLine(deviceName);
+            }
+
+            _abletonIn = new MidiReader(8);
+
+            _resolume = new Resolume(7001);
+            _resolume.Start();
+
+            // wait for a key press to exit
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+
+            _resolume.Stop();
+        }
+
+  
+    }
+}
---
+++ ResolumeController/Resolume.cs
@@ -0,0 +1,173 @@
+﻿using Rug.Osc;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResolumeController
+{
+    public delegate void ChangedEventHandler(string oscPath, object value);
+
+    
+    public abstract class ResolumeValue
+    {
+        public DateTime TimeStamp { get; protected set; } = DateTime.Now;
+        public TimeSpan Age => DateTime.Now.Subtract(TimeStamp);
+        public abstract TCast GetValueAS<TCast>() where TCast : class;
+    }
+
+    public class ResolumeValue<T> : ResolumeValue
+    {
+        private T _value;
+        public T Value {
+            get { return _value; }
+            set { TimeStamp = DateTime.Now; _value = value; }
+        }
+
+        public ResolumeValue(T value)
+        {
+            Value = value;
+        }
+
+        public override TCast GetValueAS<TCast>() 
+        {
+            return Value as TCast;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} [@{TimeStamp.Ticks}, {typeof(T).Name}]";
+        }
+    }
+
+
+    public class ResolumeValueCollection : ConcurrentDictionary<string, ResolumeValue>
+    {
+        public T GetValueAs<T>(string key)
+            where T : ResolumeValue
+        {
+            return (T)this[key];
+        }
+    }
+
+    public class Resolume
+    {
+        private readonly OscReceiver _resolumeOscReceiver;
+        private readonly Thread _listenerThread;
+        private readonly ResolumeValueCollection _values = new ResolumeValueCollection();
+
+        public ChangedEventHandler ValueChanged;
+
+        public Resolume(int listenOnPort)
+        {
+            
+            _resolumeOscReceiver = new OscReceiver(listenOnPort);
+            _listenerThread = new Thread(new ThreadStart(ListenLoop));
+        }
+
+        public void Start()
+        {
+            _resolumeOscReceiver.Connect();
+            _listenerThread.Start();
+        }
+
+        public void Stop()
+        {
+            _resolumeOscReceiver.Close();
+            _listenerThread.Join();
+        }
+
+        public ResolumeValue GetValue(string oscPath)
+        {
+            ResolumeValue v;
+            if (_values.TryGetValue(oscPath, out v))
+            {
+                return v;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void SetValue(string oscPath, ResolumeValue value)
+        {
+            _values[oscPath] = value;
+            ValueChanged?.Invoke(oscPath, value);
+        }
+
+
+        private void ListenLoop()
+        {
+            try
+            {
+                while (_resolumeOscReceiver.State != OscSocketState.Closed)
+                {
+                    // if we are in a state to recieve
+                    if (_resolumeOscReceiver.State == OscSocketState.Connected)
+                    {
+                        // get the next message, this will block until one arrives or the socket is closed
+                        OscBundle packet = _resolumeOscReceiver.Receive() as OscBundle;
+
+                        foreach (OscMessage message in packet)
+                        {
+                            var arguments = message.ToArray();
+                            if (arguments.Length != 1)
+                                throw new InvalidOperationException("More than one argument is not supported.");
+                            ResolumeValue val;
+                            if (!_values.TryGetValue(message.Address, out val))
+                            {
+                                switch (arguments[0].GetType().Name)
+                                {
+                                    case "Single":
+                                        val = new ResolumeValue<Single>((Single)arguments[0]);
+                                        break;
+                                    case "String":
+                                        val = new ResolumeValue<String>((String)arguments[0]);
+                                        break;
+                                    case "Int32":
+                                        val = new ResolumeValue<Int32>((Int32)arguments[0]);
+                                        break;
+                                    default:
+                                        throw new ApplicationException($"Type not supported: {arguments[0].GetType().FullName}");
+                                }
+                                //_values.AddOrUpdate()
+                                _values[message.Address] = val;
+                            }
+                            else
+                            {
+                                switch (arguments[0].GetType().Name)
+                                {
+                                    case "Single":
+                                        (val as ResolumeValue<Single>).Value = (Single)arguments[0];
+                                        break;
+                                    case "String":
+                                        (val as ResolumeValue<String>).Value = (String)arguments[0];
+                                        break;
+                                    case "Int32":
+                                        (val as ResolumeValue<Int32>).Value = (Int32)arguments[0];
+                                        break;
+                                    default:
+                                        throw new ApplicationException($"Type not supported: {arguments[0].GetType().FullName}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // if the socket was connected when this happens
+                // then tell the user
+                if (_resolumeOscReceiver.State == OscSocketState.Connected)
+                {
+                    Console.WriteLine("Exception in listen loop");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
